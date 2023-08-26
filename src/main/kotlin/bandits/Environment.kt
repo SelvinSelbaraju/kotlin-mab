ddd4589782bd646ffc4b5814826f260d836268b5
Selvin Selbaraju <selvinoselbaraju@gmail.com>
2023-08-26 10:39:18 +0100

    add customers to environment config

--- src/main/kotlin/bandits/Environment.kt
+++ src/main/kotlin/bandits/Environment.kt
@@ -3,6 +3,17 @@ package bandits
 import kotlinx.serialization.Serializable
 
 @Serializable
+data class CustomerConfig(
+    val customers: Map<String, CustomerStats>
+)
+
+@Serializable
+data class CustomerStats(
+    val populationProb: Double,
+    val armProbs: Map<String, Double>
+)
+
+@Serializable
 data class Arm(
     val name: String,
     val mean: Double,
@@ -12,5 +23,6 @@ data class Arm(
 data class Environment(
     val numTrials: Int,
     val numSteps: Int,
-    val arms: Array<Arm>
+    val arms: Array<Arm>,
+    val customers: Map<String, CustomerStats>
 )
