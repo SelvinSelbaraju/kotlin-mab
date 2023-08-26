173bdd044343b33e96176fa80fedd3deea9aea18
Selvin Selbaraju <selvinoselbaraju@gmail.com>
2023-08-26 22:47:15 +0100

    remove un-needed code

--- src/main/kotlin/bandits/environments/Environment.kt
+++ src/main/kotlin/bandits/environments/Environment.kt
@@ -3,11 +3,6 @@ package bandits.environments
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class CustomerConfig(
-    val customers: Map<String, CustomerStats>
-)
-
-@Serializable
 data class CustomerStats(
     val populationProb: Double,
     val armProbs: Map<String, Double>
