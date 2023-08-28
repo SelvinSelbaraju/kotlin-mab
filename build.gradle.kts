bc7b7924b8bcdfb3278443f33ae3ae23e9109cd2
Selvin Selbaraju <selvinoselbaraju@gmail.com>
2023-08-28 10:54:47 +0100

    add compose desktop as a dep

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,13 @@
 plugins {
     kotlin("jvm") version "1.9.0"
     kotlin("plugin.serialization") version "1.9.0"
+    id("org.jetbrains.compose") version "1.4.3"
 }
 
 repositories {
     mavenCentral()
+    maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+    google()
 }
 
 dependencies {
@@ -12,4 +15,11 @@ dependencies {
     implementation("org.jetbrains.lets-plot:lets-plot-kotlin-jvm:4.4.1")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.1")
     implementation("org.apache.commons:commons-math3:3.6.1")
+    implementation(compose.desktop.macos_arm64)
+}
+
+compose.desktop {
+    application {
+        mainClass = "MainKt"
+    }
 }
