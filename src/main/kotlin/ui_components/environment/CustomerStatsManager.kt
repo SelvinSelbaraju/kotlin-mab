30e74154fe3980a58d875f53a7a2eccdec14361b
Selvin Selbaraju <selvinoselbaraju@gmail.com>
2023-09-03 09:32:10 +0100

    get population prob working too

--- src/main/kotlin/ui_components/environment/CustomerStatsManager.kt
+++ src/main/kotlin/ui_components/environment/CustomerStatsManager.kt
@@ -4,27 +4,41 @@ import androidx.compose.material.Text
 import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateMapOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import bandits.environments.CustomerStats
 
 @Composable
 fun CustomerStatsManager(arms: MutableList<String>, customerList: MutableList<String>) {
-    val customerMap = customerList.map { it to arms.associateWith { 0.0 }}
+    val customerMap = customerList.map { it to CustomerStats(0.0, arms.associateWith { 0.0 })}
     val customers = remember { mutableStateMapOf(*customerMap.toTypedArray()) }
     // For every customer, create text fields for each arm
     for (customer in customers) {
         Text(customer.key)
+        TextField(
+            value = customer.value.populationProb.toString(),
+            onValueChange = { newValue ->
+                val convertedValue = newValue.toDoubleOrNull() ?: 0.0
+                val updatedCustomerStats = customer.value.copy()
+                updatedCustomerStats.populationProb = convertedValue
+                customers[customer.key] = updatedCustomerStats
+            },
+            label = { Text("Population Prob") }
+        )
         for (arm in arms) {
             TextField(
-                value = customer.value[arm].toString(),
+                value = customer.value.armProbs[arm].toString(),
                 onValueChange = { newValue ->
                     val convertedValue = newValue.toDoubleOrNull() ?: 0.0
 
                     // Create a new inner map and update the value
-                    val updatedInnerMap = customer.value.toMutableMap()
+                    val updatedCustomerStats = customer.value.copy()
+                    val updatedInnerMap = updatedCustomerStats.armProbs.toMutableMap()
                     updatedInnerMap[arm] = convertedValue
+                    updatedCustomerStats.armProbs = updatedInnerMap
 
                     // Replace the inner map in the outer state
-                    customers[customer.key] = updatedInnerMap
+                    customers[customer.key] = updatedCustomerStats
                 },
                 label = { Text(arm) }
             )
